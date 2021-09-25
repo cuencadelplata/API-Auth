095057724d5eca9ef6e3ce8f229f4045bf1935c1
Eduardo Ricciardi <eduardojavierricciardi@qgmail.com>
2021-09-24 21:49:14 -0300

    Actualización

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const morgan = require('morgan');
+
+const app = express()
+const port = 3000
+
+// middleware para loggear los eventos del servidor
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Servidor funcionando</h1>')
+})
+
+app.post('/auth/generarToken', (req, res)=> {
+    const usuario = {
+        usuario: 'Eduardo',
+        correo: 'correojuan@mail.com'
+    }
+
+    jwt.sign({usuario}, 'secretkey', (err, token)=>{
+        res.json({
+            token
+        });
+
+    });
+})
+
+app.post('/auth/verificarToken', verificarToken, (req, res)=>{
+    jwt.verify(req.token, 'secretkey', (err, authDatos)=>{
+        if(err){
+            res.sendStatus(403);
+        }else{
+            res.json({
+                mensaje: "Token válido",
+                authDatos: authDatos
+            })
+        }
+    });
+})
+
+//Authorization: Bearer <token>
+function verificarToken (req, res, next){
+    const bearerHeader = req.headers['authorization'];
+
+    if (typeof bearerHeader !== 'undefined'){
+        const bearerToken = bearerHeader.split(bearerHeader, " ")[1];
+        req.token = bearerToken;
+        next();
+    }else{
+        res.sendStatus(403);
+    }
+};
+
+
+
+app.post('/usuario', (req, res)=> {
+    res.json({
+        nombreUsuario: 'Juan',
+        password: 'miPassword'
+    });
+})
+
+app.post('/autenticar',(req, res)=>{})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
